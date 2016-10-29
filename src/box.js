ac91f02a71059419e89b6d44970e354866360715
Amalan Iyengar <amalan.iyengar@gmail.com>
2016-10-29 11:44:20 -0400

    Added grid, resized canvas

--- src/box.js
+++ src/box.js
@@ -2,7 +2,7 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth * 0.7, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -11,6 +11,14 @@ var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
+camera.position.y = 1;
+
+var size = 10;
+var step = 10;
+var gridHelper = new THREE.GridHelper(size, step);
+
+
+scene.add(gridHelper);
 
 var render = function () {
     requestAnimationFrame( render );
