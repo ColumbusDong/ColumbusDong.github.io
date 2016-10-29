eb96cf567a69cd0cd633f14853533cfd4825ed6d
ColumbusDong <columbusdong@gmail.com>
2016-10-29 12:26:42 -0400

    Grids and Sky color
    Added grids for XY, XZ, YZ planes and Sky Color

--- src/box.js
+++ src/box.js
@@ -5,8 +5,10 @@ var camera = new THREE.PerspectiveCamera( 75, width/height, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
+renderer.setClearColor (0xFFFFFF, 1);
 document.body.appendChild( renderer.domElement );
 
+//Rotating Cube
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
@@ -15,15 +17,25 @@ scene.add( cube );
 camera.position.z = 5;
 camera.position.y = 1;
 
-var size = 10;
-var step = 10;
-var gridHelper = new THREE.GridHelper(size, step);
-
 controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.target.set(0, 0, 0);
 
-scene.add(gridHelper);
+//Grid
+var size = 100;
+var step = size;
+var gridXZ = new THREE.GridHelper(size, step);
 
+var gridXY = new THREE.GridHelper(size, step);
+gridXY.rotation.x = Math.PI/2;
+
+var gridYZ = new THREE.GridHelper(size, step);
+gridYZ.rotation.z = Math.PI/2;
+
+scene.add(gridXZ);
+scene.add(gridXY);
+scene.add(gridYZ);
+
+//Make Cube Rotate
 var render = function () {
     requestAnimationFrame( render );
 
